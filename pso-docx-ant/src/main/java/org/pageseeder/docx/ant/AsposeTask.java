330a106f60942d39eedb5a620093ef98bb4357ca
philipr <philipr@weborganic.com>
2023-09-20 16:30:21 +1000

    Added aspose-words ANT task

---
+++ pso-docx-ant/src/main/java/org/pageseeder/docx/ant/AsposeTask.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 1999-2012 weborganic systems pty. ltd.
+ */
+package org.pageseeder.docx.ant;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.pageseeder.docx.util.XSLT;
+import org.pageseeder.docx.util.ZipUtils;
+import org.slf4j.Logger;
+
+import javax.mail.MessagingException;
+import javax.xml.transform.Templates;
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.aspose.words.cloud.*;
+import com.aspose.words.cloud.api.*;
+import com.aspose.words.cloud.model.*;
+import com.aspose.words.cloud.model.requests.*;
+import com.aspose.words.cloud.model.responses.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * An ANT task to convert a DOCX file to PDF using Aspose cloud
+ *
+ * @author Philip Rutherford
+ */
+public final class AsposeTask extends Task {
+
+  /**
+   * The Word document to convert.
+   */
+  private File source;
+
+  /**
+   * The PDF file to create.
+   */
+  private File destination;
+
+  /**
+   * The name of the working directory
+   */
+  private File working;
+
+  /**
+   * The configuration.
+   */
+  private File config;
+
+  /**
+   * List of parameters specified for the transformation into PSML
+   */
+  private List<Parameter> params = new ArrayList<>();
+
+  /**
+   * The Aspose cloud client ID
+   */
+  private String clientId;
+
+  /**
+   * The Aspose cloud client secret
+   */
+  private String clientSecret;
+
+  // Set properties
+  // ----------------------------------------------------------------------------------------------
+
+  /**
+   * Set the source file (a DOCX file).
+   *
+   * @param docx The Word document (DOCX) to import.
+   */
+  public void setSrc(File docx) {
+    if (!(docx.exists())) throw new BuildException("the document " + docx.getName()+ " doesn't exist");
+    if (docx.isDirectory()) throw new BuildException("the document " + docx.getName() + " can't be a directory");
+    String name = docx.getName();
+    if (!name.endsWith(".docx")) {
+      log("Word document file should generally end with .docx - but was "+name);
+    }
+    this.source = docx;
+  }
+
+  /**
+   * Set the destination The PDF file to create.
+   *
+   * @param destination The destination file.
+   */
+  public void setDest(File destination) {
+    this.destination = destination;
+  }
+
+  /**
+   * @param id the Aspose cloud client ID.
+   */
+  public void setClientId(String id) {
+    this.clientId = id;
+  }
+
+  /**
+   * @param secret the Aspose cloud client secret.
+   */
+  public void setClientSecret(String secret) {
+    this.clientSecret = secret;
+  }
+
+  // Execute
+  // ----------------------------------------------------------------------------------------------
+
+  @Override
+  public void execute() throws BuildException {
+    if (this.source == null)
+      throw new BuildException("Source document must be specified using 'src' attribute");
+    if (this.destination == null)
+      throw new BuildException("Destination document must be specified using 'dest' attribute");
+    if (this.clientId == null)
+      throw new BuildException("Client ID must be specified using 'clientid' attribute");
+    if (this.clientSecret == null)
+      throw new BuildException("Client secret must be specified using 'clientsecret' attribute");
+
+    log("Converting DOCX " + this.source.getName() + " to PDF " + this.destination.getName());
+
+    ApiClient apiClient = new ApiClient(this.clientId, this.clientSecret, null);
+    WordsApi wordsApi = new WordsApi(apiClient);
+    try {
+      byte[] requestDocument = Files.readAllBytes(this.source.toPath());
+      ConvertDocumentRequest convertRequest = new ConvertDocumentRequest(requestDocument, "pdf", null, null, null, null, null, null, null);
+      Files.write(this.destination.toPath(), wordsApi.convertDocument(convertRequest));
+    } catch (IOException | MessagingException | ApiException ex) {
+      throw new BuildException(ex);
+    }
+
+    log("Conversion complete");
+  }
+
+}
---
+++ pso-docx-ant/src/test/java/org/pageseeder/docx/ant/AsposeTaskTest.java
@@ -0,0 +1,80 @@
+package org.pageseeder.docx.ant;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.pageseeder.docx.util.Files;
+import org.xml.sax.SAXException;
+import org.xmlunit.matchers.CompareMatcher;
+
+import java.io.*;
+import java.util.Properties;
+
+public class AsposeTaskTest {
+
+  private static final File CASES = new File("src/test/aspose/cases");
+
+  private static final File RESULTS = new File("test/aspose/results");
+
+  private static final File CLIENT = new File("aspose.properties");
+
+  @Test
+  public void testBasic() throws IOException, SAXException {
+    testIndividual("basic");
+  }
+
+  @Test
+  public void testPsGenerate() throws IOException, SAXException {
+    testIndividual("ps-generated");
+  }
+  public void testIndividual(String folderName) throws IOException {
+    testIndividual(new File(CASES, folderName));
+  }
+
+  public void testIndividual(File dir) throws IOException {
+    if (dir.isDirectory()) {
+
+      if (new File(dir, dir.getName() + ".docx").exists()) {
+        System.out.println(dir.getName());
+        File result_dir = new File(RESULTS, dir.getName());
+        result_dir.mkdirs();
+        File result = new File(result_dir, dir.getName() + ".pdf");
+        File actual = process(dir, result);
+        File expected = new File(dir, "expected.pdf");
+
+        // Check that the files exist
+        Assert.assertTrue(actual.exists());
+        Assert.assertTrue(expected.exists());
+
+        Assert.assertTrue(actual.length() > 0);
+        Assert.assertTrue(expected.length() > 0);
+        Assert.assertTrue("Expected PDF file size: " + expected.length() + " but was " + actual.length(),
+            expected.length() == actual.length());
+      } else {
+        throw new IOException("Unable to find DOCX file for test:" + dir.getName());
+      }
+    }
+  }
+
+  private File process(File test, File result) throws IOException {
+    AsposeTask task = new AsposeTask();
+    task.setSrc(new File(test, test.getName() + ".docx"));
+    task.setDest(result);
+    try (InputStream input = new FileInputStream(CLIENT)) {
+      Properties props = new Properties();
+      props.load(input);
+      String clientId = props.getProperty("clientid");
+      String clientSecret = props.getProperty("clientsecret");
+      if (clientId == null || clientSecret == null) {
+        throw new IOException("The clientid and clientsecret must be defined in pso-docx-ant/aspose.properties");
+      }
+      task.setClientId(clientId);
+      task.setClientSecret(clientSecret);
+    } catch (FileNotFoundException ex) {
+      throw new IOException("The clientid and clientsecret must be defined in pso-docx-ant/aspose.properties");
+    }
+    task.execute();
+
+    return result;
+  }
+
+}
